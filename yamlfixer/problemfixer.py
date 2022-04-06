61a3407ae14a7b43048f629a15f00a0cf5c8ebb3
Jérôme Alet <jerome.alet@opt.nc>
2022-04-06 15:19:24 +1100

    Added copyright strings

--- yamlfixer/problemfixer.py
+++ yamlfixer/problemfixer.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 #
+# Copyright (C) 2021-2022 OPT-NC
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
