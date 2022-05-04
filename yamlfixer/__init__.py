872f78e2ede1d4b19afab783023662d189dd14e1
Jérôme Alet <jerome.alet@opt.nc>
2022-05-05 08:10:06 +1100

    Bump version: 0.9.6 -> 0.9.7

--- yamlfixer/__init__.py
+++ yamlfixer/__init__.py
@@ -24,7 +24,7 @@ to try to fix each of the reported problems.
 
 import time
 
-__version__ = "0.9.6"
+__version__ = "0.9.7"
 __author__ = "OPT-NC"
 __license__ = "GPLv3+"
 __year__ = time.strftime("%Y", time.localtime(time.time()))
